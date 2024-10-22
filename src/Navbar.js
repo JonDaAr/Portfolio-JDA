cf1e889bcf4cabf8872c34171a011e56f3ccabe2
Jonatan.D <Jonatan_Arroyo@outlook.com>
2024-10-22 20:09:10 -0300

    Actualizacion Star, NavBar y Style

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,17 +1,28 @@
 import React from 'react';
-import '../dist/style/style.css'
+import '../dist/style/style.css';
 
 const Navbar = () => {
+  
+  const handleClick = () => {
+    // Añadir la clase 'rotate-bg' al body para activar la animación del fondo
+    document.body.classList.add('rotate-bg');
+    
+    // Eliminar la clase después de la animación (5 segundos en este caso)
+    setTimeout(() => {
+      document.body.classList.remove('rotate-bg');
+    }, 5000); // Duración de la animación
+  };
+
   return (
     <div className="navbar">
       <div className="navbar-left">
         <h1>Jonatan Arroyo</h1>
       </div>
       <ul className="navbar-right">
-        <li><a href="#inicio">Inicio</a></li>
-        <li><a href="#sobre-mi">Sobre mí</a></li>
-        <li><a href="#proyectos">Proyectos</a></li>
-        <li><a href="#contacto">Contacto</a></li>
+        <li><a href="#inicio" onClick={handleClick}>Inicio</a></li>
+        <li><a href="#about" onClick={handleClick}>Sobre mí</a></li>
+        <li><a href="#proyectos" onClick={handleClick}>Proyectos</a></li>
+        <li><a href="#contacto" onClick={handleClick}>Contacto</a></li>
       </ul>
     </div>
   );
--- src/StarField.js
+++ src/StarField.js
@@ -1,18 +1,29 @@
 import React, { useEffect } from 'react';
-import '/dist/style/style.css'; // Asegúrate de tener el CSS para las estrellas
+import '/dist/style/style.css';
 
 const StarField = () => {
   useEffect(() => {
     const starField = document.querySelector(".star-field");
-    const starCount = 5;
+    const starCount = 5; // Número de estrellas fugaces
+    const fixedStarCount = 100; // Número de estrellas fijas
 
+    // Crear estrellas fugaces
     for (let i = 0; i < starCount; i++) {
       const star = document.createElement("div");
-      star.classList.add("star");
-      star.style.left = `${Math.random() * 100}vw`;
+      star.classList.add("star", "shooting-star");
+      star.style.left = `${Math.random() * 100}%`;
       star.style.animationDuration = `${Math.random() * 6 + 6}s`;
-      star.style.animationDelay = `${Math.random() * 0.1}s`;
+      star.style.animationDelay = `${Math.random() * 2}s`;
       starField.appendChild(star);
+    }
+
+    // Crear estrellas fijas
+    for (let i = 0; i < fixedStarCount; i++) {
+      const fixedStar = document.createElement("div");
+      fixedStar.classList.add("star", "fixed-star");
+      fixedStar.style.left = `${Math.random() * 100}%`;
+      fixedStar.style.top = `${Math.random() * 100}%`;
+      starField.appendChild(fixedStar);
     }
   }, []);
 
