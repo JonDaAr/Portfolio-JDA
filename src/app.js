db6537a49245628c7e2b7b9452aa63d3e4530325
Jonatan.D <Jonatan_Arroyo@outlook.com>
2024-10-22 20:12:22 -0300

    Agregado de Imagenes

--- src/app.js
+++ src/app.js
@@ -1,14 +1,31 @@
 import React from 'react';
 import Navbar from './Navbar';
 import StarField from './StarField.js';
-import '../dist/style/style.css'
+import Hero from './Hero.js';
+import AboutMe from './AboutMe.js';
+import Projects from './Project.js';
+import Footer from './Footer.js';
+import '../dist/style/style.css';
 
 const App = () => {
+  const handleClick = () => {
+    // Añadir la clase 'rotate-bg' al body o un contenedor para activar la animación del fondo
+    document.body.classList.add('rotate-bg');
+    
+    // Eliminar la clase después de la animación para que pueda repetirse
+    setTimeout(() => {
+      document.body.classList.remove('rotate-bg');
+    }, 5000); // La duración de la animación (5 segundos en este caso)
+  };
   return (
-    <div calss="container">
+    <div className='NavBAr'>
+      
       <StarField />
       <Navbar />
-      
+      <Hero />
+      <AboutMe />
+      <Projects />
+      <Footer />
     </div>
   );
 };
