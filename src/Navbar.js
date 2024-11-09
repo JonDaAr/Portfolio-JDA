b5c7721dbddf96d2055e0f0bcb156049a4c8145f
Jonatan.D <Jonatan_Arroyo@outlook.com>
2024-11-09 14:48:10 -0300

    Resposive Navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,24 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../dist/style/style.css';
 
 const Navbar = () => {
-  
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
 
   return (
-    <div className='conteiner_Nav'>
-    <div className="navbar">
-      <div className="navbar-left">
-        <h1>Jonatan Arroyo</h1>
+    <div className="conteiner_Nav">
+      <div className="navbar">
+        <div className="navbar-left">
+          <h1>Jonatan Arroyo</h1>
+        </div>
+        {/* Botón de hamburguesa solo para pantallas pequeñas */}
+        <button className="menu-toggle" onClick={toggleMenu}>
+          ☰
+        </button>
+        <ul className={`navbar-right ${isOpen ? 'open' : ''}`}>
+          <li><a href="#inicio" onClick={toggleMenu}>Inicio</a></li>
+          <li><a href="#about" onClick={toggleMenu}>Sobre mí</a></li>
+          <li><a href="#proyectos" onClick={toggleMenu}>Proyectos</a></li>
+          <li><a href="#contacto" onClick={toggleMenu}>Contacto</a></li>
+        </ul>
       </div>
-      <ul className="navbar-right">
-        <li><a href="#inicio" >Inicio</a></li>
-        <li><a href="#about" >Sobre mí</a></li>
-        <li><a href="#proyectos" >Proyectos</a></li>
-        <li><a href="#contacto" >Contacto</a></li>
-      </ul>
-    </div>
     </div>
   );
 };
 
 export default Navbar;
+
