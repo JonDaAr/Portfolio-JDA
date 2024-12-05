35cf51a33e1a93a8af2b80f645a362bd0271fe26
Jonatan.D <Jonatan_Arroyo@outlook.com>
2024-12-04 21:44:54 -0300

    Mejoramiento de la galeria de fotos Seccion fotografia

--- src/Gallery.js
+++ src/Gallery.js
@@ -1,27 +1,42 @@
-import React from 'react';
-import '/dist/style/Gallery.css'
+import React from "react";
+import '/dist/style/Gallery.css';
+import imagen1 from '/public/assets/gallery/imagen2.jpg';
+import imagen2 from '/public/assets/gallery/imagen7.jpg';
+import imagen3 from '/public/assets/gallery/imagen30.jpg';
+import imagen4 from '/public/assets/gallery/imagen10.png';
+import imagen5 from '/public/assets/gallery/imagen33.png';
+import imagen6 from '/public/assets/gallery/imagen28.png';
+import imagen7 from '/public/assets/gallery/imagen27.png';
+
+
 
 const Gallery = () => {
   return (
-    <div className="galery_cont">
-      <h3 className="about__content-main-title">Intereses Personales: Fotografía</h3>
-      <p className="galery--details-p">
-      La fotografía es un hobby que me permite capturar momentos y explorar la creatividad visual. Me interesa especialmente el trabajo con paisajes y la edición, buscando siempre un balance entre técnica y narrativa. A continuación, te presento una selección de algunas de mis fotos favoritas que muestran mi enfoque en este arte.</p>
-
-      <div className="container-img">
-        {[...Array(7)].map((_, index) => {
-          const imgNumber = index + 1;
-          return (
-            <div
-              key={imgNumber}
-              className={`box-img ${index === 0 ? 'box-img-child' : ''} ${index === 3 ? 'box-img-tall' : ''}`}
-            ></div>
-          );
-        })}
+    <div className="container">
+      <div className="box">
+        <img src={imagen7} alt="imagen1"/>
       </div>
-
+      <div className="box">
+      <img src={imagen2} alt="imagen2"/>
+      </div>
+      <div className="box">
+      <img src={imagen3} alt="imagen3"/>
+      </div>
+      <div className="box">
+      <img src={imagen1} alt="imagen4"/>
+      </div>
+      <div className="box">
+      <img src={imagen6} alt="imagen5"/>
+      </div>
+      <div className="box">
+      <img src={imagen4} alt="imagen6"/>
+      </div>
+      <div className="box">
+      <img src={imagen5} alt="imagen7"/>
+      </div>
     </div>
   );
 };
 
 export default Gallery;
+
