20aa5ab2ff1df6a6f456161af2b4168738a14fb8
Jonatan.D <Jonatan_Arroyo@outlook.com>
2025-01-09 23:23:07 -0300

    Modificacion de la galeria de fotos

--- src/Gallery.js
+++ src/Gallery.js
@@ -1,45 +1,38 @@
-import React from "react";
-import '/dist/style/Gallery.css';
-import imagen1 from '/public/assets/gallery/imagen2.jpg';
-import imagen2 from '/public/assets/gallery/imagen7.jpg';
-import imagen3 from '/public/assets/gallery/imagen30.jpg';
-import imagen4 from '/public/assets/gallery/imagen10.png';
-import imagen5 from '/public/assets/gallery/imagen33.png';
-import imagen6 from '/public/assets/gallery/imagen28.png';
-import imagen7 from '/public/assets/gallery/imagen27.png';
-
-
+import React, { useState } from "react";
+import "/dist/style/Gallery.css";
+import imagen1 from "/public/assets/gallery/imagen2.jpg";
+import imagen2 from "/public/assets/gallery/imagen7.jpg";
+import imagen3 from "/public/assets/gallery/imagen30.jpg";
+import imagen4 from "/public/assets/gallery/imagen10.png";
+import imagen5 from "/public/assets/gallery/imagen33.png";
+import imagen6 from "/public/assets/gallery/imagen28.png";
+import imagen7 from "/public/assets/gallery/imagen27.png";
 
 const Gallery = () => {
-  return (
-    
+  const [selectedImage, setSelectedImage] = useState(imagen1);
 
-    <div className="container">
-      
-      <div className="box">
-        <img src={imagen7} alt="imagen1"/>
+  return (
+    <div className="gallery-container">
+      <div className="central-image">
+        <img src={selectedImage} alt="Imagen central" />
       </div>
-      <div className="box">
-      <img src={imagen2} alt="imagen2"/>
-      </div>
-      <div className="box">
-      <img src={imagen3} alt="imagen3"/>
-      </div>
-      <div className="box">
-      <img src={imagen1} alt="imagen4"/>
-      </div>
-      <div className="box">
-      <img src={imagen6} alt="imagen5"/>
-      </div>
-      <div className="box">
-      <img src={imagen4} alt="imagen6"/>
-      </div>
-      <div className="box">
-      <img src={imagen5} alt="imagen7"/>
+      <div className="circle-container">
+        {[imagen1, imagen2, imagen3, imagen4, imagen5, imagen6, imagen7].map(
+          (img, index) => (
+            <div
+              key={index}
+              className={`circle-image ${
+                img === selectedImage ? "active" : ""
+              }`}
+              onClick={() => setSelectedImage(img)}
+            >
+              <img src={img} alt={`imagen${index + 1}`} />
+            </div>
+          )
+        )}
       </div>
     </div>
   );
 };
 
 export default Gallery;
-
